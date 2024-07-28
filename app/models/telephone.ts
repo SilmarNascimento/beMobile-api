947a94dbeebd9f8840900e499b91a3e464152239
SilmarNascimento <silmarfnascimento@gmail.com>
2024-07-28 17:28:09 -0300

    WIP: implementa a Model da entidade Telephone

--- app/models/telephone.ts
+++ app/models/telephone.ts
@@ -1,9 +1,17 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import Client from './client.js'
+import { BelongsTo } from '@adonisjs/lucid/types/relations'
 
 export default class Telephone extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
+
+  @column()
+  declare clientId: number
+
+  @belongsTo(() => Client)
+  declare client: BelongsTo<typeof Client>
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
